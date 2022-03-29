4268e5f95121097561b6541d4b4300b7fc2c9979
Vinicius Boscardin <boscardinvinicius@gmail.com>
2022-03-29 18:03:21 -0300

    :bug: fixed method get cors

--- adapter/http/middleware/cors.go
+++ adapter/http/middleware/cors.go
@@ -5,7 +5,7 @@ import "net/http"
 //Cors config enabled to all origin
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
-		response.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, DELETE, PUT")
+		response.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, DELETE, PUT, GET")
 		response.Header().Set("Content-Type", "application/json")
 		response.Header().Set("Access-Control-Allow-Origin", "*")
 		response.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
